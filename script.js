b12ba448f7eff45c8722bf63dd88dc4dfcc6d357
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2023-12-11 15:08:35 -0500

    only time-check left (ln 34 and 36)

--- script.js
+++ script.js
@@ -10,12 +10,19 @@ async function getUserData(id) {
     db2: db2,
     db3: db3
   };
-  const returnedValue = await central(id);
-  
-  const personal_info = await vault(id);
-  const db = dbs[returnedValue] (id);
-  return (db, personal_info)
+  try {
+    let returnedValue = await central(id);
+
+      // let personal_info = await vault(id); + let db = dbs[returnedValue] (id); in promise.all
+    const [db, personalInfo] = await Promise.all([dbs[returnedValue](id), vault(id)]);
+
+    return {db, personalInfo};
+    
+  } catch (error) {
+    return error;
+  }
 }
+
 
 //use async/await syntax to create a function
 //called getUserData that takes an id number 
@@ -24,5 +31,28 @@ async function getUserData(id) {
 
 getUserData(2)
 .then((user_info) => {
-    console.log(user_info);
-});
+  const time = new Date().getTime();
+      console.log(user_info);
+      console.log("processed completed in:", new Date().getTime() - time, "ms");
+  });
+
+// import { central, db1, db2, db3, vault } from "./databases.js";
+// async function getUserData(id) {
+//   try {
+//     const dbs = {
+//       db1: db1,
+//       db2: db2,
+//       db3: db3,
+//     };
+//     const db = await central(id);
+//     const [info, secure] = await Promise.all([dbs[db], vault(id)]);
+//     return { id, ...info, ...secure };
+//   } catch (error) {
+//     return error;
+//   }
+// }
+// (async () => {
+//   const time = new Date().getTime();
+//   console.log(await getUserData(1));
+//   console.log("processed completed in:", new Date().getTime() - time, "ms");
+// })();
